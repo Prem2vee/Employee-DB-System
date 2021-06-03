b99b073649980d9e813751e52dbb1d288b6f673c
Prince Segbefia <princeasegbefia@gmail.com>
2021-06-03 13:16:07 -0600

    Second Commit

--- app.js
+++ app.js
@@ -4,6 +4,11 @@ const app = express()
 const path = require('path')
 const dotenv = require('dotenv')
 const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+const methodOverride = require('method-override')
+const session = require('express-session')
+const flash = require('connect-flash')
+
 
 // Importing routes
 const employeeRoutes = require('./routes/employees')
@@ -18,9 +23,30 @@ mongoose.connect(process.env.DATABASE_LOCAL, {
     useCreateIndex: true
 })
 
+app.use(bodyParser.urlencoded({extended: true}))
 app.set('views, path', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
+
+// middleware to connect flash
+app.use(flash())
+
+//middleware for express session 
+app.use(session ({
+    secret: 'myfirstdatabase',
+    resave: true,
+    saveUninitialized: true
+}))
+
+//  Setting messares variable for flash globally
+app.use((req, res, next) => {
+    res.locals.success_msg = req.flash(('success_msg'))
+    res.locals.error_msg = req.flash(('error_msg'))
+    next()
+})
+
+// middleware for method override
+app.use(methodOverride('_method'))
 
 // Using routes
 app.use(employeeRoutes)
